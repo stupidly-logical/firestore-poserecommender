78d99fc1e3fda77551bd92ef01b2985fa47052bc
Abirami Sukumaran <abidsukumaran@gmail.com>
2025-01-17 09:38:20 +0530

    Update GenerateEmbeddings.java

---
+++ src/main/java/com/example/demo/GenerateEmbeddings.java
@@ -0,0 +1,106 @@
+package com.example.demo;
+
+/* Copyright 2022 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+import static java.util.stream.Collectors.toList;
+
+import com.google.cloud.aiplatform.v1.EndpointName;
+import com.google.cloud.aiplatform.v1.PredictRequest;
+import com.google.cloud.aiplatform.v1.PredictResponse;
+import com.google.cloud.aiplatform.v1.PredictionServiceClient;
+import com.google.cloud.aiplatform.v1.PredictionServiceSettings;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GenerateEmbeddings {
+  public static void main(String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    // Details about text embedding request structure and supported models are available in:
+    // https://cloud.google.com/vertex-ai/docs/generative-ai/embeddings/get-text-embeddings
+    String endpoint = "us-central1-aiplatform.googleapis.com:443";
+    String project = "<<YOUR_PROJECT_ID>>";
+    String model = "text-embedding-005";
+    predictTextEmbeddings("Yoga pose for spine correction");
+  }
+
+  // Gets text embeddings from a pretrained, foundational model.
+  public static List<Double> predictTextEmbeddings(String text) throws IOException{
+
+    String endpoint = "us-central1-aiplatform.googleapis.com:443";
+    String project = "<<YOUR_PROJECT_ID>>";
+    String model = "text-embedding-005";
+    String task = "SEMANTIC_SIMILARITY";
+    OptionalInt outputDimensionality = OptionalInt.of(768);
+    PredictionServiceSettings settings =
+        PredictionServiceSettings.newBuilder().setEndpoint(endpoint).build();
+    Matcher matcher = Pattern.compile("^(?<Location>\\w+-\\w+)").matcher(endpoint);
+    String location = matcher.matches() ? matcher.group("Location") : "us-central1";
+    EndpointName endpointName =
+        EndpointName.ofProjectLocationPublisherModelName(project, location, "google", model);
+
+    // You can use this prediction service client for multiple requests.
+    try (PredictionServiceClient client = PredictionServiceClient.create(settings)) {
+      PredictRequest.Builder request =
+          PredictRequest.newBuilder().setEndpoint(endpointName.toString());
+      if (outputDimensionality.isPresent()) {
+        request.setParameters(
+            Value.newBuilder()
+                .setStructValue(
+                    Struct.newBuilder()
+                        .putFields("outputDimensionality", valueOf(outputDimensionality.getAsInt()))
+                        .build()));
+      } 
+      
+        request.addInstances(
+            Value.newBuilder()
+                .setStructValue(
+                    Struct.newBuilder()
+                        .putFields("content", valueOf(text))
+                        .putFields("task_type", valueOf(task))
+                        .build()));
+
+      PredictResponse response = client.predict(request.build());
+      List<List<Float>> floats = new ArrayList<>();
+     // for (Value prediction : response.getPredictionsList()) {
+        Value prediction = response.getPredictions(0);
+        Value embeddings = prediction.getStructValue().getFieldsOrThrow("embeddings");
+        Value values = embeddings.getStructValue().getFieldsOrThrow("values");
+        floats.add(
+            values.getListValue().getValuesList().stream()
+                .map(Value::getNumberValue)
+                .map(Double::floatValue)
+                .collect(toList()));
+      //}
+      return values.getListValue().getValuesList().stream()
+      .map(Value::getNumberValue)
+      .collect(toList());
+    }
+  }
+
+  private static Value valueOf(String s) {
+    return Value.newBuilder().setStringValue(s).build();
+  }
+
+  private static Value valueOf(int n) {
+    return Value.newBuilder().setNumberValue(n).build();
+  }
+}
